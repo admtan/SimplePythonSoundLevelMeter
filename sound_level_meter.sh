56c69f0b5f02f6be579b74e49eda0f3c158138c0
admtan <9016499+admtan@users.noreply.github.com>
2023-09-03 00:25:29 +0900

    Create sound_level_meter.sh
    Add munin plugin

---
+++ sound_level_meter.sh
@@ -0,0 +1,17 @@
+#!/bin/dash
+
+if [ "$1" = "config" ]; then
+	echo "graph_title Sound level"
+	echo "graph_category xopts"
+	echo "soundlevel.label dBSPL"
+	exit 0
+fi
+
+if [ -e /tmp/8a32ada4c3e0c1f0.csv ]; then
+	value=$(cud -f 2 /tmp/8a32ada4c3e0c1f0.csv)
+	echo "soundlevel.value ${value}"	
+	exit 0
+else
+	echo "csv file does not exist."
+	exit 1
+fi
